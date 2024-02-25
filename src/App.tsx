ea0833c39145f7a0adbdc8314ab0b718423e11d6
Jacek <limaniec@gmail.com>
2024-02-25 09:23:51 +0100

    fixed scrolling implementation error

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,13 @@ import 'aos/dist/aos.css'
 function App() {
 
   const [navMobile, setNavMobile] = useState(false);
+
   useEffect(()=>{
     Aos.init({
-      duration:2500,
-      delay:400
+      duration:2500
     })
-  })
+  },[])
+  
   return (
     <div className="overflow-hidden relative before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:block">
       <Header setNavMobile={setNavMobile} />
